c7b5c009c606694353eac7439f5362d4e7f50c1c
Bijen Hirachan <bijen.hirachan@gmail.com>
2025-10-17 23:59:44 +0200

    Padding decreased

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,4 +1,4 @@
-  <div class="bg relative h-[80svh] text-center py-20 flex text-white justify-start lg:justify-center items-center flex-col space-y-4">
+  <div class="bg relative h-[80svh] text-center py-4 lg:py-20 flex text-white justify-start lg:justify-center items-center flex-col space-y-4">
        
         <p class="text-4xl mt-32 font-bold">Wings And Dumplings</p>
         <p class="text-lg">Zeelstraat 6, 3000 Leuven</p>
