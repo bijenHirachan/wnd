e44dcf46cec60509e5e0cdc9db32a3303333c5a7
Bijen Hirachan <bijen.hirachan@gmail.com>
2025-10-18 17:29:45 +0200

    Responsive constraint changed from lg to md

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,9 +1,9 @@
-  <div class="bg relative h-[80svh] text-center py-4 lg:py-20 flex text-white justify-start lg:justify-center items-center flex-col space-y-4">
+  <div class="bg relative h-[80svh] text-center py-8 md:py-20 flex text-white justify-start md:justify-center items-center flex-col space-y-4">
        
         <p class="text-4xl mt-32 font-bold">Wings And Dumplings</p>
         <p class="text-lg">Zeelstraat 6, 3000 Leuven</p>
 
-          <div class="bg-orange-50/10 lg:block absolute bottom-4 lg:right-4 p-4 rounded-3xl">
+          <div class="bg-orange-50/10 md:block absolute bottom-4 md:right-4 p-4 rounded-3xl">
             <h3 class="text-white text-xl">Opening hours</h3>
             <div class="grid grid-cols-2 gap-x-8 gap-y-3 mt-4 text-white">
               <span>Monday</span><span>17:00 - 02:00</span>
