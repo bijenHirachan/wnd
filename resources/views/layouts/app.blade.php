6747060bc10cade47232a40dc317b6f89af3e4b3
Bijen Hirachan <bijen.hirachan@gmail.com>
2025-10-18 18:13:02 +0200

    Favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
   <title>Wings and Dumplings</title>
 @vite('resources/css/app.css')
 <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 <style>
