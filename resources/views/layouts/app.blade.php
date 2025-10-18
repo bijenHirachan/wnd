f2cfaebdf92272cbfd3ac2ab42697259fb4c879a
Bijen Hirachan <bijen.hirachan@gmail.com>
2025-10-18 21:44:20 +0200

    Meta tag added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Wings and Dumplings takeaway restaurant in Zeelstraat 6, 3000 Leuven, Belgium.">
+  <meta name="keywords" content="Wings, Dumplings, Mo:Mo, Takeaway, Nepali momo, Leuven, Belgium, Restaurant, Food, Buffalo Wings, Korean Spicy Wings, Loempia, Chowmein, Frieten, Fries, Chicken wings, Asian Cuisine">
   <title>Wings and Dumplings</title>
 @vite('resources/css/app.css')
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+
 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
