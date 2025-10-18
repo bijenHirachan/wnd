f5f7e5a0bcd490c9d1a92f29e6348944646a4479
Bijen Hirachan <bijen.hirachan@gmail.com>
2025-10-18 22:09:52 +0200

    More keywords added on meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Wings and Dumplings takeaway restaurant in Zeelstraat 6, 3000 Leuven, Belgium.">
-  <meta name="keywords" content="Wings, Wings And Dumplings, Wings and dumplings, Oude Markt, Wings & Dumplings, Zeelstraat, Zeelstraat 6, Leuven centrum, Leuven center, Dumplings, Momo, Mo:Mo, Takeaway, Nepali momo, Leuven, Belgium, Restaurant, Food, Buffalo Wings, Korean Spicy Wings, Loempia, Chowmein, Spring rolls, Frieten, Fries, Chicken wings, Asian Cuisine">
+  <meta name="keywords" content="Wings, Wings And Dumplings, Wings and dumplings, Oude Markt, Wings & Dumplings, Zeelstraat, Zeelstraat 6, Leuven centrum, Leuven center, Dumplings, Momo, Mo:Mo, Takeaway, Nepali momo, Leuven, Belgium, Restaurant, Food, Buffalo Wings, Korean Spicy Wings, Loempia, Chicken momo, Pork momo, Pork dumplings, Chicken dumplings, Chowmein, Spring rolls, Frieten, Fries, Chicken wings, Asian Cuisine">
   <title>Wings and Dumplings</title>
 @vite('resources/css/app.css')
 <link rel="preconnect" href="https://fonts.googleapis.com">
